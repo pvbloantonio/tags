c7bf35daaf525b2135b3d5438537a631b2a538af
Pablo Elgueta <pablo.elguetav@gmail.com>
2023-07-19 15:42:01 -0400

    Input Tags

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+
+import InputTag from "./components/InputTag.vue";
+ 
+export default{
+  components: {
+    InputTag,
+  },
+  methods: {
+    handleOnTagsChange(tags){
+      console.log(tags);
+    },
+  },
+ }
+</script>
+
+<template>
+
+<InputTag @onTagsChange="handleOnTagsChange"></InputTag>
+
+</template>
+
+<style scoped>
+
+</style>
